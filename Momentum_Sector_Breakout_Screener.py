d1776ae531b4465f99e03959eed39e0c490db52c
stockscope <stockscopepro@gmail.com>
2025-05-29 12:14:17 +0400

    Update Momentum_Sector_Breakout_Screener.py

--- Momentum_Sector_Breakout_Screener.py
+++ Momentum_Sector_Breakout_Screener.py
@@ -26,10 +26,7 @@ with st.expander("🧠 **Screening Criteria Used**", expanded=True):
         - Proximity to 52W High is highlighted
     - **Displayed Metrics**:
         - Price, 1D/1W/1M Returns, Volume, Setup Type
-    """)
-# Show spinner during screening
-with st.spinner("🔍 Screening in progress... please wait"):
-    
+    """)   
 # Load CSV
 csv_url = "https://raw.githubusercontent.com/stockscope/Momentum_Sector_Breakout_Screener/main/ind_nifty500list.csv"
 df_nifty500 = pd.read_csv(csv_url)
